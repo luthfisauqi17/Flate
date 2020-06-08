19f9806055f16905c4519e0d485c4dde27aa9879
luthfisauqi17 <sauqiluthfi877@gmail.com>
2020-06-09 06:05:46 +0700

    Index styling complete

--- app/app.py
+++ app/app.py
@@ -1,21 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 notes = [
     {
         "title": "NOTES#1",
-        "content": "test"
+        "content": "test",
+        "type": "primary"
     },
     {
         "title": "NOTES#2",
-        "content": "testtoo"
+        "content": "testtoo",
+        "type": "secondary"
     }
 ]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("index.html", notes=notes)
+    return render_template("index.html", notes=notes, title="All notes")
 
 if __name__ == "__main__":
     app.run(debug=True)
