988a074895607867b30e1df0b08ce5456e86ddb4
luthfisauqi17 <sauqiluthfi877@gmail.com>
2020-06-13 19:24:47 +0700

    Adding comments

--- app/app.py
+++ app/app.py
@@ -1,11 +1,14 @@
+# Import
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+# Configuration
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///notes.db"
 db = SQLAlchemy(app)
 
+# SqlAlchemy Model
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255))
@@ -16,12 +19,15 @@ class Note(db.Model):
     def __repr__(self):
         return f"Note('{self.id}', '{self.title}', '{self.content}', '{self.note_type}', '{self.date_created}')"
 
+# Routing
+# Home
 @app.route("/")
 @app.route("/home")
 def home():
     notes = Note.query.all()
     return render_template("index.html", notes=notes, title="All notes")
 
+# New note
 @app.route("/new", methods=['GET', 'POST'])
 def new_note():
     if request.method == "POST":
@@ -34,12 +40,14 @@ def new_note():
         return redirect(url_for("home"))
     return render_template("new_note.html", title="New note")
 
+# Delete note
 @app.route("/delete/<note_id>")
 def delete_note(note_id):
     Note.query.filter_by(id=note_id).delete()
     db.session.commit()
     return redirect(url_for("home"))
 
+# Edit note
 @app.route("/edit/<note_id>", methods=['GET', 'POST'])
 def edit_note(note_id):
     if request.method == "POST":
@@ -52,10 +60,13 @@ def edit_note(note_id):
     edit_note = Note.query.filter_by(id=note_id).first()
     return render_template("edit_note.html", edit_note = edit_note, title="Edit note - " + edit_note.title)
 
+# Note view
 @app.route("/view/<note_id>")
 def view_note(note_id):
     view_note = Note.query.filter_by(id=note_id).first()
     return render_template("view_note.html", view_note = view_note, title=view_note.title)
 
+
+# Debug
 if __name__ == "__main__":
     app.run(debug=True)
