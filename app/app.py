420b37ed511098db25f1ffe2602343111fd54f2f
luthfisauqi17 <sauqiluthfi877@gmail.com>
2020-06-15 12:53:09 +0700

    Minor changes

--- app/app.py
+++ app/app.py
@@ -1,7 +1,6 @@
 # Import
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import func
 from datetime import datetime
 
 # Configuration
